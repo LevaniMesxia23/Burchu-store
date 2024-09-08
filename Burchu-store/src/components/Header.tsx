06a88e8ccef9887e1abdfbc065b08cae0fc889c6
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-09-08 21:29:21 +0400

    first commit

---
+++ Burchu-store/src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import hamburger from "../../public/images/interface.png"
+import logo from "../../public/images/logo.jpg"
+import search from "../../public/images/search.png"
+
+function Header() {
+  return (
+    <div>
+      <div className=' flex justify-between items-center bg-black w-full px-16'>
+        <img src={hamburger} alt="" className=' w-8 h-8'/>
+        <img src={logo} alt="" />
+        <img src={search} alt="" className='w-8 h-8'/>
+      </div>
+    </div>
+  )
+}
+
+export default Header
