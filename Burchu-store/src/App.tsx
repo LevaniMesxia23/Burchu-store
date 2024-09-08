5a9b218499f559fe690c474bf03aa1e98bd6ecaa
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-09-09 02:10:34 +0400

    style: create Footer component

--- Burchu-store/src/App.tsx
+++ Burchu-store/src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Products from "./components/products"
 
@@ -8,6 +9,7 @@ function App() {
     <>
      <Header />
      <Products />
+     <Footer/>
     </>
   )
 }
--- Burchu-store/src/components/products.tsx
+++ Burchu-store/src/components/products.tsx
@@ -2,12 +2,12 @@ import data from "../data.json";
 
 function Products() {
   return (
-    <div className='bg-black h-full text-white grid gap-8 p-8 md:grid-cols-2 lg:grid-cols-4'>
+    <div className='bg-black text-white grid gap-8 p-8 min-h-screen sm:grid-cols-2 lg:grid-cols-4'>
       {data.product.map((item, index) => (
-        <div key={index} className='flex flex-col items-center rounded-lg p-4'>
-          <img src={item.image} alt="" className='w-full h-auto rounded-lg mb-6'/>
-          <p className='mb-2 text-center'>{item.name}</p>
-          <p className='text-center'>₾ {item.cost} Gel</p>
+        <div key={index} className='flex flex-col items-start rounded-lg p-4'>
+          <img src={item.image} alt={item.name} className='w-full h-auto rounded-lg mb-6'/>
+          <p className='mb-2 text-center text-[2rem] text-orange-200'>{item.name}</p>
+          <p className='text-center text-[1.5rem]'>₾ {item.cost} Gel</p>
         </div>
       ))}
     </div>
