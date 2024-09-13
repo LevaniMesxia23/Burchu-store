6c7951ebdbc838ce25848d6c1c981b108eb68416
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-09-13 19:15:13 +0400

    refactor: change footer component

--- Burchu-store/src/components/products.tsx
+++ Burchu-store/src/components/products.tsx
@@ -2,12 +2,12 @@ import data from "../data.json";
 
 function Products() {
   return (
-    <div className='bg-black text-white grid gap-8 p-8 min-h-screen sm:grid-cols-2 lg:grid-cols-4'>
+    <div className=' text-white grid gap-8 p-8 min-h-screen grid-cols-2 sm:grid-cols-2 lg:grid-cols-4'>
       {data.product.map((item, index) => (
         <div key={index} className='flex flex-col items-start rounded-lg p-4'>
           <img src={item.image} alt={item.name} className='w-full h-auto rounded-lg mb-6'/>
-          <p className='mb-2 text-center text-[2rem] text-orange-200'>{item.name}</p>
-          <p className='text-center text-[1.5rem]'>₾ {item.cost} Gel</p>
+          <p className='mb-2 text-center text-[2rem] text-black'>{item.name}</p>
+          <p className='text-black text-center text-[1.5rem]'>₾ {item.cost} Gel</p>
         </div>
       ))}
     </div>
