d8b6cb746f4a1213ee3f571d21240955e91a95a7
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-09-13 18:46:40 +0400

    style: add HomePage component

--- Burchu-store/src/App.tsx
+++ Burchu-store/src/App.tsx
@@ -1,13 +1,14 @@
 import Footer from "./components/Footer"
 import Header from "./components/Header"
+import HomePage from "./components/HomePage"
 import Products from "./components/Products"
 
 function App() {
 
-
   return (
     <>
      <Header />
+     <HomePage/>
      <Products />
      <Footer/>
     </>
---
+++ Burchu-store/src/components/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainImg from "../../public/images/cr7.jpeg"
+
+function HomePage() {
+  return (
+    <div>
+      <img src={MainImg} alt="" />
+    </div>
+  )
+}
+
+export default HomePage
