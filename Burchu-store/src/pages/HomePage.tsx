3fce12df5172f0a076cfeba88d8af3893e1a697a
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-09-15 00:57:41 +0400

    feat: add Helmet

--- Burchu-store/src/pages/HomePage.tsx
+++ Burchu-store/src/pages/HomePage.tsx
@@ -1,12 +1,11 @@
 import { useContext } from "react";
-import MainImg from "../../public/images/focus.jpg";
-
+import { Helmet } from "react-helmet";
 import { Mycontext } from "../App";
 import Footer from "../components/Footer";
 import Products from "../components/Products";
 import { useTranslation } from 'react-i18next';
+import MainImg from "../../public/images/focus.jpg";
 import papi from '../../public/images/papi.jpeg';
-
 
 function HomePage() {
   const { i18n } = useTranslation();
@@ -18,16 +17,22 @@ function HomePage() {
 
   return (
     <div className={`${i18n.language === "en" ? "font-righteous" : "font-mikheil"}`}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Burchu</title>
+        <link rel="canonical" href="/images/logo.jpg" />
+        <link rel="icon" href="/images/logo.jpg" type="image/jpeg" />
+      </Helmet>
       {!isTablet ? (
-        <img src={MainImg} className=" w-full" alt="" />
+        <img src={MainImg} className="w-full" alt="Main" />
       ) : (
         <img
           src={papi}
-          className=" flex justify-center items-center w-full h-[620px]"
-          alt=""
+          className="flex justify-center items-center w-full h-[620px]"
+          alt="Papi"
         />
       )}
-      <h1 className=" flex justify-center text-[2.5rem] font-bold mt-4">
+      <h1 className="flex justify-center text-[2.5rem] font-bold mt-4">
         First Drop
       </h1>
       <Products />
